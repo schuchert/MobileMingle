b99716050cc93d8a0fb80a50b24edb8fbea595d3
bschuche <Thoughtworks@leeza-demuth.(none)>
2012-01-10 21:50:36 -0600

    Added the integration tests and updated build.gradle to build them
    Added new target, integrationTest, to run them.

---
+++ src/test/java/com/tw/mobilemingle/integration/tests/UrlReader.java
@@ -0,0 +1,35 @@
+package com.tw.mobilemingle.integration.tests;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class UrlReader {
+  public String getContent(String url) {
+    StringBuffer result = new StringBuffer();
+    try {
+      HttpClient httpclient = new DefaultHttpClient();
+      HttpGet httpget = new HttpGet(url);
+      HttpResponse response = httpclient.execute(httpget);
+      HttpEntity entity = response.getEntity();
+      if (entity != null) {
+        InputStream instream = entity.getContent();
+        InputStreamReader irs = new InputStreamReader(instream);
+        BufferedReader br = new BufferedReader(irs);
+        String l;
+        while ((l = br.readLine()) != null) {
+          result.append(l);
+        }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return result.toString();
+  }
+}
